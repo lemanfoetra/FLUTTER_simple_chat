ea18b6921e5c10cf78d6014cef832b6f8ee6ee91
Ardyn Sulaeman <lemanfoetra@gmail.com>
2021-02-07 21:14:37 +0700

    using StreamBuilder() Widget

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,25 +5,39 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, i) => Container(
-          padding: EdgeInsets.all(10),
-          child: Text('This work!'),
-        ),
+      appBar: AppBar(
+        title: Text('Chats'),
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/pCOBPMJt44ZD4Yh1Qhoz/messages')
+            .snapshots(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+              ),
+            );
+          } else {
+            final data = snapshot.data.documents;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (ctx, i) => Container(
+                padding: EdgeInsets.all(10),
+                child: Text(data[i]['text']),
+              ),
+            );
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/pCOBPMJt44ZD4Yh1Qhoz/messages')
-              .snapshots()
-              .listen((data) {
-            data.documents.forEach((document) {
-              print(document['text']);
-            });
-          });
-        },
+        onPressed: () {},
       ),
     );
   }
