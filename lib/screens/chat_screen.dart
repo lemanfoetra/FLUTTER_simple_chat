3825f6e15facb8226ae59af6360f7b9b12b29221
Ardyn Sulaeman <lemanfoetra@gmail.com>
2021-02-11 21:31:50 +0700

    Listening to Chat Messages with "StreamBuilder"

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:simple_chat/widgets/chat/message.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -40,33 +41,7 @@ class ChatScreen extends StatelessWidget {
           )
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('chats/pCOBPMJt44ZD4Yh1Qhoz/messages')
-            .snapshots(),
-        builder: (ctx, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                snapshot.error.toString(),
-              ),
-            );
-          } else {
-            final data = snapshot.data.documents;
-            return ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (ctx, i) => Container(
-                padding: EdgeInsets.all(10),
-                child: Text(data[i]['text']),
-              ),
-            );
-          }
-        },
-      ),
+      body: Message(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
---
+++ lib/widgets/chat/message.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Message extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Firestore.instance.collection('chat').snapshots(),
+      builder: (ctx, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (chatSnapshot.hasError) {
+          return Container();
+        } else {
+          final data = chatSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (ctx, i) {
+              if (data[i]['text'] == null) {
+                return Container();
+              }
+              return Text(data[i]['text']);
+            },
+          );
+        }
+      },
+    );
+  }
+}
